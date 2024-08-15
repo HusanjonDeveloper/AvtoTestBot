be38c096639f8b33cde030d954b48e3d05492f31
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-08-15 18:48:13 +0500

    Identifying the user that has crashed

--- TestBot/Services/UserService.cs
+++ TestBot/Services/UserService.cs
@@ -27,7 +27,7 @@ public class UserService
                 Role = UserRole.User
             };
 
-            if (user.ChatId == Constants.SuperAdmin)
+            if (user.ChatId == StaticService.SuperAdmin)
                 user.Role = UserRole.SuperAdmin;
             
             Users.Add(user);
